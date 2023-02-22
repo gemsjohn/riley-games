4f51514de6fa1baee069c507320a91143233bf6d
JRM <johnrhmartin@gmail.com>
2023-02-22 14:51:48 -0500

    continued UI updates for Home page

--- pages/home.tsx
+++ pages/home.tsx
@@ -13,17 +13,20 @@ export default function Home() {
 
 
       <header>
-        <Image
-          src={home_bg_0}
-          alt={"Home background 0"}
-          width='100%'
-          style={{
-            position: 'absolute',
-            zIndex: -10
-          }}
-        />
+        {/* <Image
+            src={cosmic_bg}
+            alt={"Cosmic Scramble background"}
+            className="my-20 max-h-96 max-w-full"
+            style={{
+              position: 'absolute',
+              zIndex: -10,
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+
+            }}
+          /> */}
         <div className="flex items-center justify-center height: 20vh">
-          <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8 flex flex-col justify-center items-center">
+          <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center">
             <h1 className="text-8xl font-bold tracking-tight text-white font-display">RILEY GAMES</h1>
             <h2 className="text-3xl font-bold tracking-tight text-white font-display text-center">
               Game on the go with Riley Games. Your source for mobile entertainment.
@@ -34,11 +37,11 @@ export default function Home() {
 
 
       <main>
-        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-5xl py-6 sm:px-6 lg:px-8">
           <div className="px-4 py-6 sm:px-0 flex items-center">
-            <div className="sm:h-100 rounded-lg border-4 border-dashed border-gray-200">
+            <div className="sm:h-100 ">
               <h2
-                className="sm:text-2xl lg:text-3xl font-bold tracking-tight font-display sm:w-10/12 md:w-11/12 m-8"
+                className="sm:text-2xl lg:text-3xl font-bold tracking-tight font-display sm:w-10/12 md:w-11/12 m-8 text-center"
                 style={{ color: '#fcd01f' }}
               >
                 At Riley Games, we believe that gaming should bring joy and happiness to people's lives.
@@ -50,199 +53,232 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="
-          mx-auto 
-          max-w-7xl 
-          py-6 
-          sm:px-6 
-          lg:px-8 
-          flex 
-          flex-row"
-          style={{ backgroundColor: '' }}>
-          {/* bg-gradient-to-b from-[#950c44] to-[#feb832] items-center justify-content  */}
 
-          <Image
-            src={cosmic_bg}
-            alt={"Cosmic Scramble background"}
-            className="my-20"
-            style={{
-              position: 'absolute',
-              zIndex: -10,
-              // top: '35%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              width: '100%',
-              height: 'auto',
-              // marginTop: '10vh'
 
-            }}
-          />
-          <Image
-            src={cosmic_icon}
-            alt={"Cosmic Scramble icon"}
-            style={{
-              borderRadius: '2vh',
-              height: '15vw',
-              width: '15vw',
-            }}
-          />
-          <div className="items-center justify-content" style={{ zIndex: 1 }}>
+        <div className="grid lg:grid-cols-2 place-content-center justify-items-center mx-auto max-w-5xl">
+          
+          <div className="max-w-xl py-6 sm:px-6 lg:px-8 flex justify-center" style={{ backgroundColor: '' }}>
+            <div className="px-4 py-6 sm:px-0 flex items-center">
+              <div className="sm:h-100  flex flex-col items-center justify-content">
 
-            <h2
-              className="sm:text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight font-display mx-2 mt-8 text-white"
-              // style={{ color: 'white' }}
-            >
-              COSMIC SCRAMBLE
-            </h2>
+                <Image
+                  src={cosmic_icon}
+                  alt={"Cosmic Scramble icon"}
+                  className="sm: w-24 md:w-40 lg:w-60 rounded-3xl m-2"
+                  style={{
+                    // borderRadius: '2vh',
+                    // height: 'auto',
+                    // width: '10vw',
+                  }}
+                />
+                <h2
+                  className="text-3xl font-bold tracking-tight font-display"
+                >
+                  COSMIC SCRAMBLE
+                </h2>
+                <h2
+                  className="text-4xl font-bold tracking-tight font-display m-8 text-center sm:w-80"
+                >
+                  Get ready to blast off into a thrilling adventure with Cosmic Scramble!
+                </h2>
 
-            <h2
-              className="sm:text-1xl md:text-2xl lg:text-3xl font-bold tracking-tight font-display my-1 mx-2 text-white"
-              // style={{ color: 'white' }}
-            >
-              Get ready to blast off into a thrilling adventure with Cosmic Scramble!
-            </h2>
-            <div className="flex flex-row ml-5 space-x-4" style={{backgroundColor: ''}}>
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  window.location.href = 'https://play.google.com/store/apps/details?id=com.cosmicscramble&hl=en_US&gl=US';
-                }}
-                className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white p-2 mt-2 w-40"
-                style={{
-                  borderRadius: '0.5vh',
-                  borderWidth: 2,
-                  borderColor: '#09e049',
-                  opacity: 0.9,
-                  // width: '30vw'
-                }}
-              >
-                <div>
-                  <text
-                  className="text-sm"
+
+                <div className="justify-self-auto flex flex-col align-middle" style={{ backgroundColor: '' }}>
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      window.location.href = 'https://play.google.com/store/apps/details?id=com.cosmicscramble&hl=en_US&gl=US';
+                    }}
+                    className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white p-2 w-40 m-2"
                     style={{
-                      color: 'white',
-                      // fontSize: '1.5vh',
-                      // fontWeight: 'bold',
-                      alignSelf: 'center',
+                      borderRadius: '0.5vh',
+                      borderWidth: 2,
+                      borderColor: '#09e049',
+                      opacity: 0.9,
                     }}
                   >
-                    Download Now
-                  </text>
-                </div>
-              </button>
+                    <div>
+                      <text
+                        className="text-lg"
+                        style={{
+                          color: 'white',
+                          textAlign: 'center'
+                        }}
+                      >
+                        Download Now
+                      </text>
+                    </div>
+                  </button>
 
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  window.location.href = '';
-                }}
-                className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white p-2 mt-2 w-40"
-                style={{
-                  borderRadius: '0.5vh',
-                  borderWidth: 2,
-                  borderColor: '#09e049',
-                  opacity: 0.9,
-                  // width: '30vw'
-                }}
-              >
-                <div>
-                  <text
-                  className="text-sm"
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      window.location.href = '';
+                    }}
+                    className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white p-2 w-40 m-2"
                     style={{
-                      color: 'white',
-                      // fontSize: '1.5vh',
-                      // fontWeight: 'bold',
-                      alignSelf: 'center',
+                      borderRadius: '0.5vh',
+                      borderWidth: 2,
+                      borderColor: '#09e049',
+                      opacity: 0.9,
                     }}
                   >
-                    More Info
-                  </text>
+                    <div>
+                      <text
+                        className="text-lg"
+                        style={{
+                          color: 'white',
+                          textAlign: 'center'
+                        }}
+                      >
+                        More Info
+                      </text>
+                    </div>
+                  </button>
                 </div>
-              </button>
-
-              
+              </div>
             </div>
           </div>
 
-          {/* <Image
-            src={cosmic_feature}
-            alt={"Home background 0"}
-            style={{
-              width: '40vh',
-              borderRadius: '5vh',
-              opacity: 1.0
-            }}
-          /> */}
+          <div className="max-w-xl py-6 sm:px-6 lg:px-8 flex justify-center" style={{ backgroundColor: '' }}>
+            <div className="px-4 py-6 sm:px-0 flex items-center">
+              <div className="sm:h-100  flex flex-col items-center justify-content">
+
+                <Image
+                  src={wordlit_icon}
+                  alt={"Cosmic Scramble icon"}
+                  className="sm: w-24 md:w-40 lg:w-60 rounded-3xl m-2"
+                  style={{
+                    // borderRadius: '2vh',
+                    // height: 'auto',
+                    // width: '10vw',
+                  }}
+                />
+                <h2
+                  className="text-3xl font-bold tracking-tight font-display"
+                >
+                  WORDLIT
+                </h2>
+                <h2
+                  className="text-4xl font-bold tracking-tight font-display m-8 text-center sm:w-80"
+                  style={{ backgroundColor: '' }}
+                >
+                  The fast-paced game that tests your word-solving skills.
+                </h2>
+
+
+                <div className="justify-self-auto flex flex-col align-middle" style={{ backgroundColor: '' }}>
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      window.location.href = 'https://play.google.com/store/apps/details?id=com.WordLit&hl=en_US&gl=US';
+                    }}
+                    className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white p-2 w-40 m-2"
+                    style={{
+                      borderRadius: '0.5vh',
+                      borderWidth: 2,
+                      borderColor: '#09e049',
+                      opacity: 0.9,
+                    }}
+                  >
+                    <div>
+                      <text
+                        className="text-lg"
+                        style={{
+                          color: 'white',
+                          textAlign: 'center'
+                        }}
+                      >
+                        Download Now
+                      </text>
+                    </div>
+                  </button>
+
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      window.location.href = '';
+                    }}
+                    className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white p-2 w-40 m-2"
+                    style={{
+                      borderRadius: '0.5vh',
+                      borderWidth: 2,
+                      borderColor: '#09e049',
+                      opacity: 0.9,
+                    }}
+                  >
+                    <div>
+                      <text
+                        className="text-lg"
+                        style={{
+                          color: 'white',
+                          textAlign: 'center'
+                        }}
+                      >
+                        More Info
+                      </text>
+                    </div>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div className="
-          mx-auto 
-          max-w-7xl 
-          py-6 
-          sm:px-6 
-          lg:px-8 
-          
-          flex 
-          flex-col
-          items-center
-          justify-content
-        ">
-          {/* bg-gradient-to-b from-[#950c44] to-[#feb832]  */}
-          <Image
-            src={wordlit_icon}
-            alt={"Cosmic Scramble icon"}
-            style={{
-              borderRadius: '2vh',
-              height: '8vh',
-              width: '8vh',
-            }}
-          />
-          <h2
-            className="sm:text-4xl lg:text-3xl font-bold tracking-tight font-display m-6"
-            style={{ color: 'white' }}
-          >
-            WORDLIT
-          </h2>
+        <div className="mx-auto max-w-5xl py-6 sm:px-6 lg:px-8  flex flex-col items-center justify-center">
+          <div className="px-4 py-6 sm:px-0 flex items-center">
+            <div className="sm:h-100 ">
+              <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+                <h1 className="text-8xl font-bold tracking-tight text-white font-display flex items-center justify-center" >BLOG</h1>
+              </div>
+              
 
-          <h2
-            className="sm:text-2xl lg:text-3xl font-bold tracking-tight font-display m-2 text-center"
-            style={{ color: 'white' }}
-          >
-            The fast-paced game that tests your word-solving skills.
-          </h2>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              window.location.href = 'https://play.google.com/store/apps/details?id=com.WordLit&hl=en_US&gl=US';
-            }}
-            className="bg-indigo-500 px-8 m-8"
-            style={{
-              borderRadius: '1vh'
-            }}
-          >
-            <div >
-              <text
+              <div 
+                className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-3xl m-8" 
                 style={{
-                  color: 'white',
-                  fontSize: '2vh',
-                  fontWeight: 'bold',
-                  alignSelf: 'center',
+                  height: '10vh',
+                  width: '70vw'
                 }}
               >
-                Download Now
-              </text>
+              </div>
+
+              <div 
+                className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-3xl m-8" 
+                style={{
+                  height: '10vh',
+                  width: '70vw'
+                }}
+              >
+              </div>
+
+              <div 
+                className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-3xl m-8" 
+                style={{
+                  height: '10vh',
+                  width: '70vw'
+                }}
+              >
+              </div>
             </div>
-          </button>
-          <Image
-            src={wordlit_feature}
-            alt={"Home background 0"}
-            style={{
-              width: '40vh',
-              borderRadius: '5vh',
-              opacity: 1.0
-            }}
-          />
+          </div>
         </div>
+
+
+        {/* #101010 */}
+
+
+        <div 
+          className="py-6 sm:px-6 lg:px-8  flex flex-col items-center justify-center" 
+          style={{backgroundColor: '#101010'}}
+        >
+          <div className="px-4 py-6 sm:px-0 flex items-center">
+          <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+                <h1 className="text-8xl font-bold tracking-tight text-white font-display flex items-center justify-center" >CONTACT</h1>
+              </div>
+          </div>
+        </div>
+
+
 
       </main>
     </>
