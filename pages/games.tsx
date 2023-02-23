4e5d6e89d0d94c62bd17e10706fbc6175ed139a6
JRM <johnrhmartin@gmail.com>
2023-02-23 12:09:18 -0500

    game page UI updates, focused on wordlit section

--- pages/games.tsx
+++ pages/games.tsx
@@ -6,6 +6,10 @@ import cosmic_example_0 from '../public/cosmic_example_0.png'
 import cosmic_example_1 from '../public/cosmic_example_1.png'
 import cosmic_example_2 from '../public/cosmic_example_2.png'
 import wordlit_feature from '../public/wordlit_feature.png'
+import wordlit_example_0 from '../public/wordlit_example_0.png'
+import wordlit_example_1 from '../public/wordlit_example_1.png'
+import wordlit_example_2 from '../public/wordlit_example_2.png'
+
 
 
 export default function Games() {
@@ -27,6 +31,7 @@ export default function Games() {
 
       <main>
 
+        {/* COSMIC SCRAMBLE */}
         <div
           className="flex flex-col justify-center items-center text-left"
           style={{ backgroundColor: '#101010' }}
@@ -36,6 +41,32 @@ export default function Games() {
             alt={"Cosmic Scramble feature graphic"}
             className="sm:w-10/12 md:w-10/12 lg:w-8/12"
           />
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = 'https://play.google.com/store/apps/details?id=com.cosmicscramble&hl=en_US&gl=US';
+            }}
+            className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white p-2 w-60 m-2"
+            style={{
+              borderRadius: '0.5vh',
+              borderWidth: 2,
+              borderColor: '#09e049',
+              opacity: 0.9,
+              position: 'relative'
+            }}
+          >
+            <div>
+              <text
+                className="text-3xl font-oswald"
+                style={{
+                  color: 'white',
+                  textAlign: 'center'
+                }}
+              >
+                Download Now
+              </text>
+            </div>
+          </button>
           <div className="flex flex-col sm:w-10/12 md:w-10/12 lg:w-8/12 mb-5">
             <h2 className="text-3xl font-bold tracking-tight font-oswald my-5">
               COSMIC SCRAMBLE
@@ -56,37 +87,51 @@ export default function Games() {
           <div className="flex flex-row flex-wrap items-center justify-center mb-5">
             <Image
               src={cosmic_example_0}
-              alt={"Cosmic Scramble example 0"}
+              alt={"Cosmic Scramble example image"}
               className="sm:w-4/12 md:w-4/12 lg:w-2/12 m-2"
               style={{ borderRadius: '10px' }}
             />
             <Image
               src={cosmic_example_1}
-              alt={"Cosmic Scramble example 0"}
+              alt={"Cosmic Scramble example image"}
               className="sm:w-4/12 md:w-4/12 lg:w-2/12 m-2"
               style={{ borderRadius: '10px' }}
             />
             <Image
               src={cosmic_example_2}
-              alt={"Cosmic Scramble example 0"}
+              alt={"Cosmic Scramble example image"}
               className="sm:w-4/12 md:w-4/12 lg:w-2/12 m-2"
               style={{ borderRadius: '10px' }}
             />
 
           </div>
 
+          
+        </div>
+
+        {/* WORDLIT */}
+        <div
+          className="flex flex-col justify-center items-center text-left"
+          style={{ backgroundColor: '' }}
+        >
+          
+          <Image
+            src={wordlit_feature}
+            alt={"Cosmic Scramble feature graphic"}
+            className="sm:w-10/12 md:w-10/12 lg:w-8/12"
+          />
           <button
             onClick={(e) => {
               e.preventDefault();
-              window.location.href = 'https://play.google.com/store/apps/details?id=com.cosmicscramble&hl=en_US&gl=US';
+              window.location.href = 'https://play.google.com/store/apps/details?id=com.WordLit&hl=en_US&gl=US';
             }}
-            className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white p-2 w-60 m-2 mb-20"
+            className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white p-2 w-60 m-2"
             style={{
               borderRadius: '0.5vh',
               borderWidth: 2,
               borderColor: '#09e049',
               opacity: 0.9,
-              position: 'absolute'
+              position: 'relative',
             }}
           >
             <div>
@@ -101,18 +146,59 @@ export default function Games() {
               </text>
             </div>
           </button>
+          <div className="flex flex-col sm:w-10/12 md:w-10/12 lg:w-8/12 mb-5">
+            <h2 className="text-3xl font-bold tracking-tight font-oswald my-5">
+              WORDLIT
+            </h2>
+            <h2 className="text-l font-bold tracking-tight font-subtext m-1">
+              The fast-paced game that tests your word-solving skills. 
+            </h2>
+            <h2 className="text-l font-bold tracking-tight font-subtext m-1 text-yellow-400">
+              Has around 2,000 English words with definitions.
+            </h2>
+            <h2 className="text-l font-bold tracking-tight font-subtext m-1">
+            You'll be tasked with uncovering two hidden words by 
+            guessing the correct letters. With only 12 guesses to work with, you'll need to 
+            use your vocabulary skills and attention to detail to uncover the words and rack 
+            up points. Need a little help? Click on the question marks to reveal a hint about the words' meanings.
+            </h2>
+          </div>
+          <div className="flex flex-row flex-wrap items-center justify-center mb-5">
+            <Image
+              src={wordlit_example_0}
+              alt={"Wordlit example image"}
+              className="w-3/12 m-2"
+              style={{ borderRadius: '10px' }}
+            />
+            <Image
+              src={wordlit_example_1}
+              alt={"Wordlit example image"}
+              className="w-3/12 m-2"
+              style={{ borderRadius: '10px' }}
+            />
+            <Image
+              src={wordlit_example_2}
+              alt={"Wordlit example image"}
+              className="w-3/12 m-2"
+              style={{ borderRadius: '10px' }}
+            />
+
+          </div>
+
+          
         </div>
 
-
-        <div
-          style={{ backgroundColor: '' }}
+        <div 
+          className="py-6 sm:px-6 lg:px-8  flex flex-col items-center justify-center" 
+          style={{backgroundColor: '#101010'}}
         >
-          <Image
-            src={wordlit_feature}
-            alt={"WordLit feature graphic"}
-            className="sm: w-64 md:w-80 lg:w-96"
-          />
+          <div className="px-4 py-6 sm:px-0 flex items-center">
+          <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+                <h1 className="text-8xl font-bold tracking-tight text-white font-oswald flex items-center justify-center" >CONTACT</h1>
+              </div>
+          </div>
         </div>
+
       </main>
     </>
   )
